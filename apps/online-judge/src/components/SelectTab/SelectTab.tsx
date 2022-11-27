41229887a9afb21b3f574b4a1bdd02186f7b2cd5
moretall <seongho4996@naver.com>
2022-11-27 18:32:15 +0900

    선택한 탭 백그라운드 설정

--- apps/online-judge/src/components/SelectTab/SelectTab.tsx
+++ apps/online-judge/src/components/SelectTab/SelectTab.tsx
@@ -1,22 +1,32 @@
 // @flow
 import * as React from 'react';
+import cx from 'classnames';
 import {isScoreViewType, ScoreView, ScoreViewNaming} from "@online-judge/domain";
 import styles from './SelectTab.module.scss';
+import {useState} from "react";
 
 type Props = {
   onSelected: (value: ScoreView) => void;
 }
 
 export const SelectTab = (props: Props) => {
+
+  const [selected, setSelected] = useState<ScoreView>('single');
+
   return (
-    <div>
+    <div className={styles['main']}>
       {Object.entries(ScoreViewNaming).map(([key, value]) => {
         return <div
           key={key}
-          className={styles['item']}
+          className={cx({
+            [styles['item']]: true,
+            [styles['item--selected']]: key === selected
+          })}
           onClick={() => {
+
           if(isScoreViewType(key)){
             props.onSelected(key);
+            setSelected(key);
             return;
           }
 
